38beca9c9f9d6b7ffed9c3f93b860edb9d53c9ed
lina-22 <haquefahinur@gmail.com>
2022-10-15 01:48:26 +0200

    Bismillahir Rahmanir Rahim Java Spring boot Project start

---
+++ src/main/java/com/javaproject/javaBack0/JavaBack0Application.java
@@ -0,0 +1,13 @@
+package com.javaproject.javaBack0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBack0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBack0Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaproject/javaBack0/JavaBack0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.javaBack0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBack0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
