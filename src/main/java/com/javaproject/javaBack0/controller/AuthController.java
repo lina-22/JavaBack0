dc89984b4fa093a58bcfbc4f03263323d543689c
lina-22 <haquefahinur@gmail.com>
2022-10-15 02:48:45 +0200

    Auth Controller Get request test succesfully

--- src/main/java/com/javaproject/javaBack0/controller/AuthController.java
+++ src/main/java/com/javaproject/javaBack0/controller/AuthController.java
@@ -1,9 +1,17 @@
 package com.javaproject.javaBack0.controller;
 
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class AuthController {
 
+
+    @RequestMapping(value = "/welcome1",method = RequestMethod.GET)
+    public String welcome(){
+          return "you are welcome";
+    }
 }
